9244f00a1cf7bfc022879df74f9840dcc00ac737
dimirhouse <dimirhouse@gmail.com>
2017-10-18 17:02:42 +0200

    siema elo

---
+++ src/DrugaKlasa.java
@@ -0,0 +1,26 @@
+public class DrugaKlasa {
+
+    int x;
+    double u;
+
+    public DrugaKlasa(int x, double u) {
+        this.x = x;
+        this.u = u;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public double getU() {
+        return u;
+    }
+
+    public void setU(double u) {
+        this.u = u;
+    }
+}
