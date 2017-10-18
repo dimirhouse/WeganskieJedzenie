89c1e8fad19daeb262d175b714bb56eed46a0e47
dimirhouse <dimirhouse@gmail.com>
2017-10-18 16:56:27 +0200

    Weganski commit
    opis jakis

---
+++ src/WeganskieJedzenie.java
@@ -0,0 +1,7 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class WeganskieJedzenie {
+    public static void main(String[] args) {
+        System.out.println("fuck world!");
+    }
+}
